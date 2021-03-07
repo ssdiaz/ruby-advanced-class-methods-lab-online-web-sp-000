32e078266e4eedc62a3496c615c032be2f36292f
Sam D <samidiaz3@gmail.com>
2021-03-07 22:58:33 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -53,11 +53,12 @@ class Song
      # filename = "Taylor Swift - Blank Space.mp3"
     song_array = filename.split(/\-|\./)  # splits string into array by - or .
     #song_array.pop                        # removes the .mp3 tag ... may not be neccessary
+    artist_name = song_array[0].strip
+    name = song_array[1].strip
 
     song = self.new
-    song.artist_name = song_array[0].strip
-    song.name = song_array[1].strip
-
+    song.name = name
+    song.artist_name = artist_name
     #self.create#_by_name(name)
 
       end
