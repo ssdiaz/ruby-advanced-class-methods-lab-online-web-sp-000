82482a82661bb829f6bbff45f7912992e4683ec6
Sam D <samidiaz3@gmail.com>
2021-03-07 21:57:25 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,31 @@ class Song
     self.class.all << self
   end
 
+  def self.create   # instantiates a song with a name property
+    song = self.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(name)    # instantiates a song with a name property
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)   # instantiates and saves a song with a name property
+    song = self.new
+    song.name = name
+    song.save
+    song
+  end
+
+  def self.find_by_name(name)    # can find a song present in @@all by name (FAILED - 1)
+                                #  returns falsey when a song name is not present in @@all (FAILED - 2)
+    self.all.collect {|song| song.name = name }
+    #end
+
+
+  end
+
 end
