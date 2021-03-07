031ef35a439fd724fea6feee5f5f3e5c1a3a9563
Sam D <samidiaz3@gmail.com>
2021-03-07 22:51:45 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -56,8 +56,8 @@ class Song
     artist_name = song_array[0].strip
     name = song_array[1].strip
 
-    #self.create_by_name(name)
-    self.song.new
+    self.create# _by_name(name)
+
       end
 
 
